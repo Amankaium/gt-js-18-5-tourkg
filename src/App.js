413d7fd4195537dbb263bd299c6f3758e4d00155
Amankaium <kaium@list.ru>
2022-07-18 09:51:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import TourList from './components/TourList';
+import {useState} from 'react';
+import Mode from './components/Mode/Mode'
+
 
 function App() {
+  const [mode, setMode] = useState({current: "day"})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <Mode mode={mode} changeMode={setMode}/>
+    <TourList mode={mode}/>
+  </div>
+  )
 }
 
 export default App;
