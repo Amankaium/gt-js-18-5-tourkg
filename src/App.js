b39442001da0cfe9aaaeba6dafb3130d9fab3b3d
Amankaium <kaium@list.ru>
2022-07-27 18:55:16 +0600

    hw mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-import TourList from './components/TourList';
 import {useState} from 'react';
-import Mode from './components/Mode/Mode'
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import Profile from './components/Profile/Profile';
+import Main from './components/Main';
 
 
 function App() {
   const [mode, setMode] = useState({current: "day"})
 
   return (
-  <div className="App">
-    <h1>hello from Earth 2!</h1>
-    <h2>Что-нибудь нормальное</h2>
-    <Mode mode={mode} changeMode={setMode}/>
-    <TourList mode={mode}/>
+  <div>
+    <BrowserRouter>
+      <nav>
+          <Link to='/'>Домой</Link>&nbsp;
+          <Link to='/profile'>Профиль</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element = {<Main mode={mode}/>}/>
+        <Route path='/profile' element={<Profile mode={mode} setMode={setMode}/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   </div>
   )
 }
